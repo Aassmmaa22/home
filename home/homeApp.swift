ceff504862de20eb4bbfa6641eee5096748ed0bd
Aassmmaa22 <aaalanzi22@twq.idserve.net>
2023-01-08 10:23:43 +0300

    Initial Commit

---
+++ home/homeApp.swift
@@ -0,0 +1,17 @@
+//
+//  homeApp.swift
+//  home
+//
+//  Created by Asma Abdulkreem on 15/06/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct homeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
